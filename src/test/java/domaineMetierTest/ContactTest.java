03389a4a1a26975e867f395988b3f01d6ba1b3b2
KodKlod <codingklod@gmail.com>
2017-04-06 08:44:44 +0200

    avant Hibernate

--- src/test/java/domaineMetierTest/ContactTest.java
+++ src/test/java/domaineMetierTest/ContactTest.java
@@ -14,12 +14,15 @@ public class ContactTest {
 
 	@Test
 	public void testRecupContactIDEgal1() {
+		// Les trois lignes qui suivent sont des incontournables.
 		EntityManagerFactory createEntityManagerFactory = Persistence
 				.createEntityManagerFactory("BaseContacts");
-
+		// EntityManager ne peut être un Singleton
 		EntityManager createEntityManager = createEntityManagerFactory
 				.createEntityManager();
 
+		// Le .find répond au R du CRUD
+		// Voir pour les CUD ce qui est disponible
 		Contact contactRecupere = createEntityManager.find(Contact.class, 1L);
 
 		assertEquals(1, contactRecupere.getId());
